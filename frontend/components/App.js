3325ad8ee3b6eca1f274f18fe3ddb2a98af3ed53
Courtney Johnson <151663544+WARSW0RD@users.noreply.github.com>
2025-01-13 18:02:38 -0600

    Add files via upload

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
+
+
 
 
 const month = Math.floor(Math.random() * 12)
@@ -7,13 +10,46 @@ const day = Math.floor(Math.random() * 28)
 const Akey = 'eLK4saZKxM8hct76YbR0hNscxLLn7WWkQArWUea2' 
 const web = `https://api.nasa.gov/planetary/apod?api_key=${Akey}`
 
+const StyledTitle = styled.h2`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 0%;
+  color: #93005a;
+  
+`
+
+const StyledP = styled.p`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  padding-bottom: 3%;
+
+`
+
+const StyledPhoto = styled.img`
+  border-top: 2px solid white;
+  padding: 0%;
+  width: 600px;
+  height: 500px;
+  float: none;
+  margin: auto;
+  border: 5px solid white;
+  border-image: fill;
+  margin-bottom: 2rem;
+  
+  
+`
+
+
 function Card({title, text, url, date}) {
   return (
 
     <div className="APOD">
-        <h2 className="pTitle">{title}</h2>
-        <p>Captured on {date}</p>
-        <img className="photo" src={url} />
+        <StyledTitle className="pTitle">{title}</StyledTitle>
+        <StyledP>Captured on {date}</StyledP>
+        <StyledPhoto className="photo" src={url} />
         <p className="descr">{text}</p>
     </div>
     )
